45552cb09164a95788b17946348f2ce0ad94ce79
GorseB <josiahkerr@hotmail.com>
2016-09-21 20:04:32 +1200

    Updated with string Formmatting

--- Website/main.js
+++ Website/main.js
@@ -3,7 +3,26 @@ function myFunction() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("worker").innerHTML = this.responseText;
+            var str = this.responseText;
+            var obj = JSON.parse(str);
+            document.getElementById("worker").innerHTML = x;
+            switch(x) {
+                case "USD":
+                    document.getElementById("worker").innerHTML = obj.rates.USD;
+                    break;
+                case "AUD":
+                    document.getElementById("worker").innerHTML = obj.rates.AUD;
+                    break;
+                case "EUR":
+                    document.getElementById("worker").innerHTML = obj.rates.EUR;
+                    break;
+                case "CAD":
+                    document.getElementById("worker").innerHTML = obj.rates.CAD;
+                    break;
+                default:
+                    break;
+            }
+
         }
     };
     xhttp.open("GET", "http://api.fixer.io/latest?base=NZD;symbols=" + x, true);
