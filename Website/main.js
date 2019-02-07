ceb973140b325d8ce9fa1fcfd118eb5eb734487f
Josiah Kerr <josiahkerr@hotmail.com>
2019-02-07 17:24:39 +1300

    Fixed Program

--- Website/main.js
+++ Website/main.js
@@ -1,30 +1,29 @@
-function myFunction() {
-    var x = document.getElementById('combo').value;
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var str = this.responseText;
-            var obj = JSON.parse(str);
-            document.getElementById("worker").innerHTML = x;
-            switch(x) {
-                case "USD":
-                    document.getElementById("worker").innerHTML = obj.rates.USD;
-                    break;
-                case "AUD":
-                    document.getElementById("worker").innerHTML = obj.rates.AUD;
-                    break;
-                case "EUR":
-                    document.getElementById("worker").innerHTML = obj.rates.EUR;
-                    break;
-                case "CAD":
-                    document.getElementById("worker").innerHTML = obj.rates.CAD;
-                    break;
-                default:
-                    break;
-            }
-
-        }
-    };
-    xhttp.open("GET", "http://api.fixer.io/latest?base=NZD;symbols=" + x, true);
-    xhttp.send();
+function myFunction () {
+  var x = document.getElementById('combo').value
+  var xhttp = new XMLHttpRequest()
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var str = this.responseText
+      var obj = JSON.parse(str)
+      document.getElementById('worker').innerHTML = x
+      switch (x) {
+        case 'USD':
+          document.getElementById('worker').innerHTML = obj.rates.USD
+          break
+        case 'AUD':
+          document.getElementById('worker').innerHTML = obj.rates.AUD
+          break
+        case 'NZD':
+          document.getElementById('worker').innerHTML = obj.rates.NZD
+          break
+        case 'CAD':
+          document.getElementById('worker').innerHTML = obj.rates.CAD
+          break
+        default:
+          break
+      }
+    }
+  }
+  xhttp.open('GET', 'http://data.fixer.io/api/latest?access_key=b58326c82022e4356cd37dd6d3ffde87&symbols=' + x, true)
+  xhttp.send()
 }
